3173567c39efa33e5b93efb5b5a600fcf9048f64
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-11-08 22:12:08 -0500

    MVP almost met -- Stretch almost implemented too with search form -- Starting Unit Testing for the react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,47 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import Card from './components/Card';
+import Search from './components/Search';
+import axios from 'axios';
 import './App.css';
 
 export default class App extends Component {
 
+  constructor(props) {
+    super();
+
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/players')
+      .then((res) => {
+        this.setState({
+          users: res.data
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  getUser = (searchInput) => {
+    this.setState({ search: searchInput })
+    fetch(`http://localhost:5000/api/players/${searchInput}`)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ users: res.data })
+      })
+      .catch(err => console.log(err))
+    }
+
+  render() {
+    return (
+      <div className='App'>
+        <Search getUser={this.getUser} />
+        <Card user={this.state.users} />
+      </div>
+    )
+  }
 }
---
+++ client/src/components/Search.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+export default class Search extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: ''
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState !== this.state.input) {}
+  }
+
+  handleChange = e => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.getUser(this.state.input)
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search Players..."
+            value={this.state.input}
+            ref={input => this.search = input}
+            onChange={this.handleChange}
+          />
+        </form>
+      </div>
+    )
+  }
+}
